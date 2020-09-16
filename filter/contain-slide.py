7b51d80ae411416ce3ccd9ae407e170fec88c198
Martti Louhivuori <martti.louhivuori@csc.fi>
2020-09-16 13:34:46 +0300

    Horisontal rule generates an empty slide

--- filter/contain-slide.py
+++ filter/contain-slide.py
@@ -7,6 +7,11 @@ def contain(key, value, format, meta):
         if 'data-background-size' not in [x[0] for x in value[1][2]]:
             value[1][2].append([u'data-background-size', u'contain'])
         return Header(value[0], value[1], value[2])
+    elif key == 'HorizontalRule':
+        name = "section"
+        attr = [[u'data-background', u'empty-slide'],
+                [u'data-background-size', u'contain']]
+        return Header(1, (name, [], attr), [])
 
 if __name__ == '__main__':
     toJSONFilter(contain)
