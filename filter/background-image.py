0162f71387c520989efdb33da5ad373cb5437aa1
Martti Louhivuori <martti.louhivuori@csc.fi>
2022-05-31 14:47:45 +0300

    Reorder reST handling

--- filter/background-image.py
+++ filter/background-image.py
@@ -28,14 +28,17 @@ def cscify(key, value, format, meta):
     # reST: special class name in a container Div triggers the same as above,
     #       but only the modified Header is returned
     elif key == 'Div' and value[1][0]['t'] == 'Header':
-        for key in ['title', 'author', 'section']:
-            if key in value[0][1]:
-                header = value[1][0]['c']
-                header[1][1].append(key)
-                if key == 'title':
-                    key = key + '-' + lang
-                header[1][2].append([u'data-background-image', template % key])
-                return Header(header[0], header[1], header[2])
+        header = value[1][0]['c']
+        if 'data-background-image' not in [x[0] for x in header[1][2]]:
+            for key in ['title', 'author', 'section']:
+                if key in value[0][1]:
+                    header[1][1].append(key)
+                    if key == 'title':
+                        key = key + '-' + lang
+                    header[1][2].append(
+                            [u'data-background-image', template % key])
+                    break
+        return Header(header[0], header[1], header[2])
 
 
 if __name__ == '__main__':
