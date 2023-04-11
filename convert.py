793a9724fe1e7c7c4b1961806f8f944d12c58f78
Tuomas Rossi <tuomas.rossi@csc.fi>
2023-04-11 06:50:23 +0000

    Enable custom theme path

--- convert.py
+++ convert.py
@@ -14,6 +14,9 @@ import subprocess
 
 from pathlib import Path
 
+slidefactory_root = Path(os.environ['SLIDEFACTORY_ROOT'])
+slidefactory_is_custom = os.environ['SLIDEFACTORY_ROOT'] != '/slidefactory'
+
 
 def run(run_args, *, verbose=False, dry_run=False):
     run_args = [str(a) for a in run_args]
@@ -39,27 +42,31 @@ def error(msg, code=1):
     print('')
     print(inspect.cleandoc(msg))
     print('')
-    if code == 1: # setup error (invalid path etc.)
-        print('Please see README.md for installation instructions.')
     sys.exit(code)
 
 
-def get_themes(path_themes):
-    """Find existing presentation themes."""
-    try:
-        themes = [x for x in os.listdir(path_themes)
-                  if os.path.isdir(os.path.join(path_themes, x))]
-    except OSError:
-        error('Invalid theme path: {0}'.format(path_themes))
-    return themes
+def find_theme(theme):
+    is_custom = False
+    if os.sep in str(theme):
+        is_custom = True
+        p = Path(theme)
+        if not p.is_dir():
+            error(f'Nonexistent theme directory {p.absolute()}')
+    else:
+        theme_root = slidefactory_root / 'theme'
+        p = theme_root / theme
+        if not p.is_dir():
+            available_themes = [str(x.name) for x in theme_root.iterdir() if x.is_dir()]
+            error(f'Invalid theme {theme}.'
+                  f' Available themes: {", ".join(available_themes)}.')
+    for fname in ['defaults.yaml', 'template.html', 'csc.css']:
+        if not (p / fname).is_file():
+            error(f'File {fname} missing from the theme directory'
+                  f' {p.absolute()}')
+    return p, is_custom
 
 
 def main():
-    root = Path(os.environ['SLIDEFACTORY_ROOT'])
-    theme_root = Path(os.environ['SLIDEFACTORY_THEME_ROOT'])
-    available_themes = get_themes(theme_root)
-    default_theme = 'csc-2016'
-
     parser = argparse.ArgumentParser(description="""Convert a presentation
     from Markdown (or reStructuredText) to reveal.js powered HTML5 using
     pandoc.""")
@@ -68,10 +75,8 @@ def main():
     parser.add_argument('--output', metavar='prefix',
             help='prefix for output filenames (by default uses the '
             'basename of the input file, i.e. talk.md -> talk.html)')
-    parser.add_argument('-t', '--theme', default=default_theme,
-            choices=available_themes, metavar='THEME',
-            help=(f'presentation theme: {", ".join(available_themes)}'
-                  f' (default: {default_theme})'))
+    parser.add_argument('-t', '--theme', metavar='THEME', default='csc-2016',
+            help=(f'presentation theme (default: %(default)s)'))
     parser.add_argument('-p', '--pdf', action='store_true', default=False,
             help='convert HTMLs to PDFs')
     parser.add_argument('-c', '--self-contained',
@@ -99,8 +104,11 @@ def main():
         contained = ''
 
     # convert files
-    theme_dpath = theme_root / args.theme
-
+    theme_dpath, is_custom_theme = find_theme(args.theme)
+    if is_custom_theme or slidefactory_is_custom:
+        theme_url = f'file://{theme_dpath.absolute()}/csc.css'
+    else:
+        theme_url = f'https://cdn.jsdelivr.net/gh/csc-training/slidefactory/theme/{args.theme}/csc.css'
     for filename in args.input:
         html = filename.with_suffix('.html')
         if args.output:
@@ -110,14 +118,13 @@ def main():
             'pandoc',
             f'--defaults={theme_dpath / "defaults.yaml"}',
             f'--template={theme_dpath / "template.html"}',
-            f'--metadata-file={root / "urls.yaml"}',
-            f'--metadata=theme-url:https://cdn.jsdelivr.net/gh/csc-training/slidefactory/theme/{args.theme}/csc.css',
+            f'--metadata-file={slidefactory_root / "urls.yaml"}',
+            f'--metadata=theme-url:{theme_url}',
             f'--output={html}',
             filename,
             ]
         run_args += [f'--filter={f}' for f in args.filter]
         run(run_args, verbose=args.verbose, dry_run=args.dry_run)
-
 
         if args.pdf:
             pdf = html.with_suffix('.pdf')
